79a4a4a513c4a9e56a5fa3c3f1e7ac1bfb55e899
jaderosse <jademrosse@gmail.com>
2017-12-22 11:18:03 -0800

    coded along

--- controllers/teams.js
+++ controllers/teams.js
@@ -18,11 +18,32 @@ router.get('/new', function(req, res) {
   res.render('teams/new');
 });
 
+router.get('/edit/:name', function(req, res){
+	// res.send('edit form');
+	var team = teamService.getTeam(req.params.name);
+	res.render('teams/edit', {team: team});
+});
+
 router.get('/:name', function(req, res) {
   // search for the team name in all the teams.
   var team = teamService.getTeam(req.params.name);
 
   res.render('teams/show', { team: team });
+});
+
+//stubbing out routes 
+router.put('/:name', function(req, res){
+	// console.log('name:', req.params.name);
+	// console.log('req.body is', req.body);
+	teamService.editTeam(req.params.name, req.body);
+	res.send('success');
+});
+
+router.delete('/:name', function(req, res){
+	// console.log('name:', req.params.name);
+	// res.send('DELETE route');
+	teamService.deleteTeam(req.params.name);
+	res.send('success');
 });
 
 module.exports = router;
--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,33 @@
 $(document).ready(function() {
+	//adding this
+	$('.delete-link').click(function(e){
+		e.preventDefault();
+		// console.log('click event was reached');
+		//AJAX call goes here	
 
+		$.ajax({
+			method: "DELETE",
+			url: $(this).attr('href')
+		}).done(function(response){
+			// console.log('promise reached');
+			window.location.href='/teams';
+		});
+	});
+
+	$('.edit-form').submit(function(e){
+		e.preventDefault();
+		// console.log('edit form ajax function');
+
+		$.ajax({
+			method: "PUT",
+			url: $(this).attr('action'),
+			data: {
+				name: $('#new-name').val(),
+				members: $('#members').val()
+			}
+		}).done(function(response){
+			//console.log('got to the promise');
+			window.location.href = '/teams';
+		});
+	});
 });
